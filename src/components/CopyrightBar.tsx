f3b53da4eaeb59c7f1ae77f0a7b8e71a4e7143ac
PTdave01 <daniyanmoses.k@gmail.com>
2026-06-13 09:02:47 +0100

    Create CopyrightBar.tsx

---
+++ src/components/CopyrightBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CopyrightBar: React.FC = () => {
+  return (
+    <div className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between text-sm text-[#051A24]">
+      <span>NoT_tHaT_kInD_oF_pHySiO</span>
+      <span>Lagos, Nigeria</span>
+    </div>
+  );
+};
+
+export default CopyrightBar;
